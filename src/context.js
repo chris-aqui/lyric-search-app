fdf4c9e298565ead6b668f47cdb496c2f7f75289
Christine Aqui <2739361+christine-aqui@users.noreply.github.com>
2018-09-05 16:25:12 -0400

    adding context for working with the state

---
+++ src/context.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react'
+
+const Context = React.createContext();
+
+export class Provider extends Component {
+    state = {
+        track_list : [
+            {track: {Track_name: "abc"}},
+            {track: {Track_name: "123"}}
+        ],
+        heading:"Top 10 Tracks"
+    }
+  render() {
+    return (
+        <Context.Provider value={this.state}>
+            {this.props.children}
+            
+        </Context.Provider>
+    )
+  }
+}
+
+
+export const Consumer = Context.Consumer;
+
+// /Context.Provider
+//  privder will wrap around every other component.
+//  This allows you at access the state though other compoents as long as consumer is used. 
