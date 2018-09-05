ceb7a91e2df86fc7f36f86f976635b91a8c4da3a
Christine <aqui.christine@gmail.com>
2018-09-04 22:53:25 -0400

    check point

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import Navbar from './Components/layout/Navbar'
 
 import './App.css';
 
@@ -6,7 +7,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        {/*  */}
+        <Navbar />
       </div>
     );
   }
