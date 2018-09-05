ec8e1490764724281bf3de4dd4f810580293f6de
Christine Aqui <2739361+christine-aqui@users.noreply.github.com>
2018-09-05 15:04:35 -0400

    added browser router route and switch

--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 import Navbar from './Components/layout/Navbar'
-
+import Index from './Components/layout/Index'
 import './App.css';
 
 class App extends Component {
   render() {
-    return (
-      <div className="App">
-        <Navbar />
-      </div>
+    return ( 
+      <Router>
+        <React.Fragment>
+          <Navbar />
+          {/*  the rest will be in containers for diff routes */}
+          <div className='container'>
+            <Switch>
+              <Route exact path="/" component={Index} />
+            </Switch>
+          </div>
+        </React.Fragment>
+      </Router>
     );
   }
 }
